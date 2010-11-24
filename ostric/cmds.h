7ccf6d0ad41933f9bf863d06c519793b6b4c252e
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-11-24 22:56:21 +0000

    Import command definitions

--- ostric/cmds.h
+++ ostric/cmds.h
@@ -1,4 +1,9 @@
 #include <stdint.h>
 
+#define GW_CMD_USE_TOKEN	0
+#define GW_CMD_REQ_TOKEN	1
+#define GW_CMD_HAVE_TOKEN	2
+#define GW_CMD_GEN_TOKEN	3
+
 int bus_command(uint8_t *buffer, int len);
 int gateway_command(uint8_t *buffer, int len);
