49c45cab316b7e0fe30efa23a6b76f29a072463f
Robert Spanton <rspanton@zepler.net>
2010-11-25 03:57:12 +0000

    Switch to a state machine for controlling the bus enumeration

--- sricd/sric-enum.c
+++ sricd/sric-enum.c
@@ -6,6 +6,22 @@
 
 static uint8_t reset_count = 0;
 
+typedef enum {
+	S_IDLE,
+	S_RESETTING,
+	S_ENUMERATED,
+} enum_state_t;
+
+typedef enum {
+	/* Start enumeration */
+	EV_START_ENUM,
+	/* The bus has been reset */
+	EV_BUS_RESET,
+} enum_event_t;
+
+/* State machine for enumeration */
+void sric_enum_fsm( enum_event_t ev );
+
 /* Append a reset command */
 static void send_reset( void )
 {
@@ -60,14 +76,40 @@ static gboolean send_reset_timeout( void* _rs )
 	send_reset();
 
 	(*rs)++;
-	if( *rs > 5 )
+	if( *rs > 5 ) {
+		sric_enum_fsm( EV_BUS_RESET );
 		return FALSE;
+	}
 	return TRUE;
+}
+
+void sric_enum_fsm( enum_event_t ev )
+{
+	static enum_state_t state = S_IDLE;
+
+	switch( state ) {
+
+	case S_IDLE:
+		if( ev == EV_START_ENUM ) {
+			g_timeout_add( 50, send_reset_timeout, &reset_count );
+			state = S_RESETTING;
+		}
+		break;
+
+	case S_RESETTING:
+		if( ev == EV_BUS_RESET ) {
+			state = S_ENUMERATED;
+		}
+		break;
+
+	case S_ENUMERATED:
+		break;
+	}
 }
 
 void sric_enum_start( void )
 {
-	g_timeout_add( 50, send_reset_timeout, &reset_count );
+	sric_enum_fsm( EV_START_ENUM );
 }
 
 bool sric_enum_rx( void )
