96691ec92891ce4c93e99e7ae4627b8db4d71701
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-11-25 02:05:52 +0000

    Outline how gateway commands are going to be handled

--- ostric/cmds.c
+++ ostric/cmds.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "ostric.h"
+#include "cmds.h"
+
 void
 bus_command(uint8_t *buffer, int len)
 {
@@ -14,7 +17,20 @@ void
 gateway_command(uint8_t *buffer, int len)
 {
 
-        printf("Unimplemented: gateway commands\n");
-        abort();
-}
+	if (len < 5) {
+		fprintf(stderr, "Gateway command has invalid length %d\n", len);
+		return;
+	}
 
+	switch (buffer[4]) {
+	case GW_CMD_USE_TOKEN:
+	case GW_CMD_REQ_TOKEN:
+	case GW_CMD_HAVE_TOKEN:
+	case GW_CMD_GEN_TOKEN:
+	default:
+		fprintf(stderr, "Unimplemented gateway cmd %d\n", buffer[4]);
+		break;
+	}
+
+	return;
+}
