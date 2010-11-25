cae6cf175293e7f03556310d4725e5802e82ab7b
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-11-25 01:30:45 +0000

    Add code for generic sric client

---
+++ ostric/generic.c
@@ -0,0 +1,19 @@
+#include "cmds.h"
+
+/* That is, the code for some generic sric bus client */
+
+void
+generic_enum_callback(struct ostric_client *this, uint8_t *buffer, int len,
+			uint8_t **resp, int *rlen)
+{
+
+	return;
+}
+
+void
+generic_msg_callback(struct ostric_client *this, uint8_t *buffer, int len,
+			uint8_t **resp, int *rlen)
+{
+
+	return;
+}
