a8915c7822f2508b1fffd4b66c3be6b816c7ec3f
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-11-25 02:21:22 +0000

    Have generic client pump message command out when it receives something

--- ostric/generic.c
+++ ostric/generic.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "ostric.h"
 #include "generic.h"
 
@@ -8,5 +10,7 @@ generic_msg(struct ostric_client *this, uint8_t *buffer, int len,
 			uint8_t **resp, int *rlen)
 {
 
+	printf("Generic client received command %X for addr %X, from %X\n",
+					buffer[4], buffer[1], buffer[2]);
 	return;
 }
