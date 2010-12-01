8de7c512616c5d9e356b73e516a84796dca95d31
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-12-01 15:47:13 +0000

    Adjust logic error - expect addr info replies to have ack bit set.

--- sricd/sric-enum.c
+++ sricd/sric-enum.c
@@ -277,7 +277,7 @@ bool sric_enum_rx( packed_frame_t *f )
 		sric_enum_fsm(EV_NEW_DEV_ACK);
 
 	/* Otherwise it can only be a response to addr info. */
-	} else if (!(f->dest_address & 0x80) && f->payload_len == 1) {
+	} else if ((f->dest_address & 0x80) && f->payload_len == 1) {
 		addr_info_replies[f->source_address & 0x7F] = true;
 		device_classes[f->source_address & 0x7F] = f->payload[0];
 		sric_enum_fsm(EV_ADDR_INFO_ACK);
