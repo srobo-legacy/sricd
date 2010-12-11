530e6beffca19018054c99451a2eff6d9041882c
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-12-11 02:46:19 +0000

    Add a short pysric example to pump a simple motor command

---
+++ pysric/examples/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# A short example that sets motor power for device 3 to 0x20
+
+import sys
+sys.path.append( os.path.join( os.path.dirname(__file__), "../" ) )
+import pysric
+
+ctl = pysric.pysric()
+
+txframe = pysric.sric_frame()
+txframe.address = 3
+txframe.note = -1
+txframe.payload_length = 3
+txframe.payload[0] = 0
+txframe.payload[1] = 32
+txframe.payload[2] = 0
+rxframe = ctl.txrx(txframe)
+
+print "bees: " + str(rxframe.address) + " " + str(rxframe.payload_length)
