eddde290ec7b173070e153393f97f22654afed9f
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-12-11 00:19:08 +0000

    Link python module against libsric, open sric context on setup

--- libsric/pysric.c
+++ libsric/pysric.c
@@ -1,5 +1,9 @@
 #include <Python.h>
 
+#include "sric.h"
+
+static sric_context sric_ctx;
+
 static PyMethodDef pysric_methods[] = {
 {	NULL,	NULL,	0,	NULL	}
 };
@@ -8,6 +12,12 @@ PyMODINIT_FUNC
 initpysric(void)
 {
 	PyObject *m;
+
+	sric_ctx = sric_init();
+	if (sric_ctx == NULL) {
+		fprintf(stderr, "Unable to initialize SRIC library\n");
+		abort();
+	}
 
 	m = Py_InitModule("pysric", pysric_methods);
 	return;
