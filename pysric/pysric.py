f6d09c73d1b8a2b4e5b6ea088e8b00f402e2dd34
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-12-11 02:16:21 +0000

    On creation of pysric object, enumerate devices into a list
    Also, saw my knees off for the horrible way python interfaces
    with pointers.

--- pysric/pysric.py
+++ pysric/pysric.py
@@ -1,13 +1,27 @@
 from ctypes import *
 
+class sric_device(Structure):
+	_fields_ = [("address", c_int), ("type", c_int)]
+
 libsric = cdll.LoadLibrary("../libsric/libsric.so")
 libsric.sric_init.argtypes = []
 libsric.sric_init.restype = c_void_p
 libsric.sric_quit.argtypes = [c_void_p]
 libsric.sric_quit.restype = None
+libsric.sric_enumerate_devices.argtypes = [c_void_p, c_void_p]
+libsric.sric_enumerate_devices.restype = POINTER(sric_device)
 
 class pysric(object):
 	sric_ctx = libsric.sric_init()
+
+	devices = []
+	tmpdev = None
+	while True:
+		tmpdev = libsric.sric_enumerate_devices(sric_ctx, tmpdev)
+		if cast(tmpdev, c_void_p).value == None:
+			break
+		# Hello, a device
+		devices.append(tmpdev[0])
 	
 	def __del__(self):
 		libsric.sric_quit(self.sric_ctx)
