c643c5ff55a23088d5b8a08809984d0278d6fc6d
Robert Spanton <rspanton@zepler.net>
2011-02-10 02:21:46 +0000

    Update the pysric example to the new API

--- pysric/examples/test.py
+++ pysric/examples/test.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python
-
 # A short example that sets motor power for device 3 to 0x20
-
-import sys
+import sys, os
 sys.path.append( os.path.join( os.path.dirname(__file__), "../" ) )
 import pysric
 
 ctl = pysric.PySric()
-
-txframe = pysric.SricFrame()
-txframe.address = 3
-txframe.note = -1
-txframe.payload_length = 3
-txframe.payload[0] = 0
-txframe.payload[1] = 32
-txframe.payload[2] = 0
-rxframe = ctl.txrx(txframe)
+motor = ctl.devices[ pysric.SRIC_CLASS_MOTOR ][0]
+rxframe = motor.txrx( [0, 32, 0 ] )
 
 print "bees:", rxframe.address, rxframe.payload_length
 
